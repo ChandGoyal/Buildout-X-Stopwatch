dd1cb56859969090b648ca9c336617d8e31c610f
ChandGoyal <chandgoyal017@gmail.com>
2024-01-28 18:32:45 +0530

    Create Stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [timeElapsed, setTimeElapsed] = useState(0);
+
+  const timeFormat = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${mins}:${`${remainingSeconds}`.padStart(2, 0)}`;
+  };
+
+  const toggleTimer = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTimeElapsed(0);
+  };
+
+  useEffect(() => {
+    // const ac = new AbortController();
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTimeElapsed((prevTimeElapsed) => prevTimeElapsed + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+      // ac.abort();
+    };
+  }, [isRunning]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time: {timeFormat(timeElapsed)}</p>
+      <button onClick={toggleTimer}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
